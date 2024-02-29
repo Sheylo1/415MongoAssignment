4989d5bcefbd05bf7d8eaf2855eba8cccc7e8b16
Sheylo1 <wesley.austin-2@selu.edu>
2024-02-29 13:20:05 -0600

    fix

--- testmongo.js
+++ testmongo.js
@@ -35,8 +35,8 @@ console.log("Looking for: " + searchKey);
 
 async function run() {
   try {
-    const database = client.db('ckmdb');
-    const parts = database.collection('cmps415');
+    const database = client.db('WesleyDB');
+    const parts = database.collection('MyStuff');
 
     // Hardwired Query for a part that has partID '12345'
     // const query = { partID: '12345' };
